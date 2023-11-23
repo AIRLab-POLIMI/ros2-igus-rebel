b18e9bbb14892e982013451a63907e18bc3a2065
SimonGiampy <simonegiampa99@gmail.com>
2023-11-23 12:03:07 +0100

    added support for octomap and 3d sensing

--- igus_rebel_moveit_config/launch/moveit_controller.launch.py
+++ igus_rebel_moveit_config/launch/moveit_controller.launch.py
@@ -7,11 +7,11 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from launch.substitutions import (
-	PathJoinSubstitution,
-	Command,
-	FindExecutable,
-	LaunchConfiguration,
-	PythonExpression,
+    PathJoinSubstitution,
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PythonExpression,
 )
 from launch_ros.substitutions import FindPackageShare
 from launch.actions import DeclareLaunchArgument
@@ -21,15 +21,15 @@ from launch.conditions import IfCondition
 
 
 def load_yaml(package_name, file_path):
-	"""Load a yaml file from the specified package"""
-	full_path = os.path.join(get_package_share_directory(package_name), file_path)
-	try:
-		with open(full_path, "r") as file:
-			return yaml.safe_load(file)
-	except (
-		EnvironmentError
-	):  # parent of IOError, OSError *and* WindowsError where available
-		return None
+    """Load a yaml file from the specified package"""
+    full_path = os.path.join(get_package_share_directory(package_name), file_path)
+    try:
+        with open(full_path, "r") as file:
+            return yaml.safe_load(file)
+    except (
+        EnvironmentError
+    ):  # parent of IOError, OSError *and* WindowsError where available
+        return None
 
 
 # use this script for launching everything in a single terminal window
@@ -40,212 +40,222 @@ def load_yaml(package_name, file_path):
 
 # launches only the URDF version 2 robot description
 def generate_launch_description():
-	gripper_arg = DeclareLaunchArgument(
-		name="gripper",
-		default_value="none",
-		choices=["none", "camera"],
-		description="Gripper mount to attach to the last joint",
-	)
+    gripper_arg = DeclareLaunchArgument(
+        name="gripper",
+        default_value="none",
+        choices=["none", "camera"],
+        description="Gripper mount to attach to the last joint",
+    )
 
-	hardware_protocol_arg = DeclareLaunchArgument(
-		name="hardware_protocol",
-		default_value="simulation",
-		choices=["mock_hardware", "cri", "simulation"],
-		description="Which hardware protocol or mock hardware should be used",
-	)
+    hardware_protocol_arg = DeclareLaunchArgument(
+        name="hardware_protocol",
+        default_value="simulation",
+        choices=["mock_hardware", "cri", "simulation"],
+        description="Which hardware protocol or mock hardware should be used",
+    )
 
-	rviz_file_arg = DeclareLaunchArgument(
-		name="rviz_file",
-		default_value="none",
-		description="Path to the RViz configuration file",
-	)
+    rviz_file_arg = DeclareLaunchArgument(
+        name="rviz_file",
+        default_value="none",
+        description="Path to the RViz configuration file",
+    )
 
-	load_base_arg = DeclareLaunchArgument(
-		name="load_base",
-		default_value="false",
-		description="Load the mobile robot model and tower",
-		choices=["true", "false"],
-	)
+    load_octomap_arg = DeclareLaunchArgument(
+        name="load_octomap",
+        default_value="false",
+        description="Load the octomap server inside the planning scene",
+        choices=["true", "false"],
+    )
 
-	return LaunchDescription(
-		[
-			gripper_arg,
-			hardware_protocol_arg,
-			rviz_file_arg,
-			load_base_arg,
-			OpaqueFunction(function=launch_setup),
-		]
-	)
+    load_base_arg = DeclareLaunchArgument(
+        name="load_base",
+        default_value="false",
+        description="Load the mobile robot model and tower",
+        choices=["true", "false"],
+    )
+
+    return LaunchDescription(
+        [
+            gripper_arg,
+            hardware_protocol_arg,
+            rviz_file_arg,
+            load_octomap_arg,
+            load_base_arg,
+            OpaqueFunction(function=launch_setup),
+        ]
+    )
 
 
 def launch_setup(context, *args, **kwargs):
+    robot_description_file = PathJoinSubstitution(
+        [
+            FindPackageShare("igus_rebel_description_ros2"),
+            "urdf",
+            "igus_rebel.urdf.xacro",  # baseline version of the robot
+        ]
+    )
 
-	robot_description_file = PathJoinSubstitution(
-		[
-			FindPackageShare("igus_rebel_description_ros2"),
-			"urdf",
-			"igus_rebel.urdf.xacro",  # baseline version of the robot
-		]
-	)
+    robot_description_content = Command(
+        [
+            FindExecutable(name="xacro"),
+            " ",
+            robot_description_file,
+            " hardware_protocol:=",
+            LaunchConfiguration("hardware_protocol"),
+            " gripper:=",
+            LaunchConfiguration("gripper"),
+            " load_base:=",
+            LaunchConfiguration("load_base"),
+        ]
+    )
 
-	robot_description_content = Command(
-		[
-			FindExecutable(name="xacro"),
-			" ",
-			robot_description_file,
-			" hardware_protocol:=",
-			LaunchConfiguration("hardware_protocol"),
-			" gripper:=",
-			LaunchConfiguration("gripper"),
-			" load_base:=",
-			LaunchConfiguration("load_base"),
-		]
-	)
+    robot_description = {
+        "robot_description": ParameterValue(robot_description_content, value_type=str)
+    }
 
-	robot_description = {
-		"robot_description": ParameterValue(robot_description_content, value_type=str)
-	}
+    robot_description_semantic_file = PathJoinSubstitution(
+        [
+            FindPackageShare("igus_rebel_moveit_config"),
+            "config",
+            "igus_rebel.srdf.xacro",
+        ]
+    )
 
-	robot_description_semantic_file = PathJoinSubstitution(
-		[FindPackageShare("igus_rebel_moveit_config"), "config", "igus_rebel.srdf.xacro"]
-	)
+    robot_description_semantic_content = Command(
+        [
+            FindExecutable(name="xacro"),
+            " ",
+            robot_description_semantic_file,
+            " gripper:=",
+            LaunchConfiguration("gripper"),
+            " load_base:=",
+            LaunchConfiguration("load_base"),
+        ]
+    )
 
-	robot_description_semantic_content = Command(
-		[
-			FindExecutable(name="xacro"),
-			" ",
-			robot_description_semantic_file,
-			" gripper:=",
-			LaunchConfiguration("gripper"),
-			" load_base:=",
-			LaunchConfiguration("load_base"),
-		]
-	)
+    robot_description_semantic = {
+        "robot_description_semantic": ParameterValue(
+            robot_description_semantic_content, value_type=str
+        )
+    }
 
-	robot_description_semantic = {
-		"robot_description_semantic": ParameterValue(
-			robot_description_semantic_content, value_type=str
-		)
-	}
+    ros2_controllers_file = PathJoinSubstitution(
+        [
+            FindPackageShare("igus_rebel_moveit_config"),
+            "config",
+            "ros2_controllers.yaml",
+        ]
+    )
 
-	ros2_controllers_file = PathJoinSubstitution(
-		[
-			FindPackageShare("igus_rebel_moveit_config"),
-			"config",
-			"ros2_controllers.yaml",
-		]
-	)
+    ompl_planning_yaml = load_yaml(
+        "igus_rebel_moveit_config", "config/ompl_planning.yaml"
+    )
+    ompl_planning_pipeline_config = {"move_group": {}}
+    ompl_planning_pipeline_config["move_group"].update(ompl_planning_yaml)
 
-	ompl_planning_yaml = load_yaml(
-		"igus_rebel_moveit_config", "config/ompl_planning.yaml"
-	)
-	ompl_planning_pipeline_config = {"move_group": {}}
-	ompl_planning_pipeline_config["move_group"].update(ompl_planning_yaml)
+    planning_scene_monitor_parameters = {
+        "publish_planning_scene": True,
+        "publish_geometry_updates": True,
+        "publish_state_updates": True,
+        "publish_transforms_updates": True,
+    }
 
-	planning_scene_monitor_parameters = {
-		"publish_planning_scene": True,
-		"publish_geometry_updates": True,
-		"publish_state_updates": True,
-		"publish_transforms_updates": True,
-	}
+    kinematics_yaml = load_yaml("igus_rebel_moveit_config", "config/kinematics.yaml")
+    kinematics = {"robot_description_kinematics": kinematics_yaml}
 
-	kinematics_yaml = load_yaml("igus_rebel_moveit_config", "config/kinematics.yaml")
-	kinematics = {"robot_description_kinematics": kinematics_yaml}
+    moveit_controllers_yaml = load_yaml(
+        "igus_rebel_moveit_config", "config/moveit_controllers.yaml"
+    )
 
-	moveit_controllers_yaml = load_yaml(
-		"igus_rebel_moveit_config", "config/moveit_controllers.yaml"
-	)
+    # unused
+    move_it_yaml = load_yaml("igus_rebel_moveit_config", "config/moveit_py.yaml")
 
-	# unused
-	move_it_yaml = load_yaml("igus_rebel_moveit_config", "config/moveit_py.yaml")
+    joint_limits_yaml = load_yaml(
+        "igus_rebel_moveit_config", "config/joint_limits.yaml"
+    )
+    joint_limits = {"robot_description_planning": joint_limits_yaml}
 
-	joint_limits_yaml = load_yaml(
-		"igus_rebel_moveit_config", "config/joint_limits.yaml"
-	)
-	joint_limits = {"robot_description_planning": joint_limits_yaml}
+    if LaunchConfiguration("load_octomap").perform(context) == "true":
+        sensors_3d_yaml = load_yaml(
+            "igus_rebel_moveit_config", "config/sensors_3d.yaml"
+        )
+    else:
+        sensors_3d_yaml = {"sensors:": ""}
 
-	#TODO: wait for the developers to make Moveit2 compatible with octomap server 2
-	sensors_3d_yaml = load_yaml(
-		"igus_rebel_moveit_config", "config/sensors_3d.yaml"
-	)
-	octomap_config = {'octomap_frame': '/octomap_point_cloud_centers', 
-                      'octomap_resolution': 0.05,
-                      'max_range': 20.0}
 
-	move_group_node = Node(
-		package="moveit_ros_move_group",
-		executable="move_group",
-		output="screen",
-		parameters=[
-			robot_description,
-			robot_description_semantic,
-			kinematics,
-			joint_limits,
-			ompl_planning_pipeline_config,
-			moveit_controllers_yaml,
-			planning_scene_monitor_parameters,
-			octomap_config,
-			#sensors_3d_yaml, #TODO: not supported yet
-		],
-		arguments=["--ros-args", "--log-level", "info"],
-	)
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[
+            robot_description,
+            robot_description_semantic,
+            kinematics,
+            joint_limits,
+            ompl_planning_pipeline_config,
+            moveit_controllers_yaml,
+            planning_scene_monitor_parameters,
+            sensors_3d_yaml,
+        ],
+        arguments=["--ros-args", "--log-level", "info"],
+    )
 
-	control_node = Node(
-		package="controller_manager",
-		executable="ros2_control_node",
-		parameters=[robot_description, ros2_controllers_file],
-		output="screen",
-	)
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, ros2_controllers_file],
+        output="screen",
+    )
 
-	robot_state_publisher_node = Node(
-		package="robot_state_publisher",
-		executable="robot_state_publisher",
-		name="robot_state_publisher",
-		parameters=[robot_description],
-		output="both",
-	)
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        parameters=[robot_description],
+        output="screen",
+    )
 
-	joint_state_broadcaster_node = Node(
-		package="controller_manager",
-		executable="spawner",
-		arguments=[
-			"joint_state_broadcaster",
-			"--controller-manager",
-			"/controller_manager",
-		],
-	)
+    joint_state_broadcaster_node = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
 
-	rebel_arm_controller_node = Node(
-		package="controller_manager",
-		executable="spawner",
-		arguments=[
-			"rebel_arm_controller",
-			"--controller-manager",
-			"/controller_manager",
-		],
-	)
+    rebel_arm_controller_node = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "rebel_arm_controller",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
 
-	rviz2_node = Node(
-		package="rviz2",
-		executable="rviz2",
-		name="rviz2",
-		output="log",
-		arguments=["-d", LaunchConfiguration("rviz_file")],
-		parameters=[
-			robot_description,
-			robot_description_semantic,
-			kinematics,
-			joint_limits,
-		],
-		condition=IfCondition(PythonExpression([ "'", LaunchConfiguration("rviz_file"), "' != 'none' "])),
-	)
-	
+    rviz2_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", LaunchConfiguration("rviz_file")],
+        parameters=[
+            robot_description,
+            robot_description_semantic,
+            kinematics,
+            joint_limits,
+        ],
+        condition=IfCondition(
+            PythonExpression(["'", LaunchConfiguration("rviz_file"), "' != 'none' "])
+        ),
+    )
 
-	return [
-		rviz2_node,
-		joint_state_broadcaster_node,
-		rebel_arm_controller_node,
-		move_group_node,
-		control_node,
-		robot_state_publisher_node,
-	]
+    return [
+        rviz2_node,
+        joint_state_broadcaster_node,
+        rebel_arm_controller_node,
+        move_group_node,
+        control_node,
+        robot_state_publisher_node,
+    ]
