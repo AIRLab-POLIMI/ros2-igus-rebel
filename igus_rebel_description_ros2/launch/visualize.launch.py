19e18c71eedcb28f7b4d5c4dbece1dcaec329a8e
SimonGiampy <simonegiampa99@gmail.com>
2024-02-04 13:07:57 +0100

    new URDF structure with support for new cameras, mounts, end effectors

--- igus_rebel_description_ros2/launch/visualize.launch.py
+++ igus_rebel_description_ros2/launch/visualize.launch.py
@@ -1,144 +1,199 @@
-# Based on the visualize_franka script licensed under the Apache license which can be found here:
-# https://github.com/frankaemika/franka_ros2/blob/develop/franka_description/launch/visualize_franka.launch.py
 
-from random import choice
 from launch import LaunchDescription
 from launch.substitutions import (
-	Command,
-	FindExecutable,
-	PathJoinSubstitution,
-	LaunchConfiguration,
+    Command,
+    FindExecutable,
+    PathJoinSubstitution,
+    LaunchConfiguration,
 )
 from launch_ros.substitutions import FindPackageShare
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.actions import OpaqueFunction
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
-	gripper_arg = DeclareLaunchArgument(
-		name="gripper",
-		default_value="none",
-		choices=["none", "camera"],
-		description="Which gripper mount to attach to the flange",
-	)
+    load_base_arg = DeclareLaunchArgument(
+        name="load_base",
+        default_value="false",
+        description="Load the mobile robot model and tower",
+        choices=["true", "false"],
+    )
 
-	hardware_protocol_arg = DeclareLaunchArgument(
-		name="hardware_protocol",
-		default_value="simulation",
-		choices=["mock_hardware", "cri", "simulation"],
-		description="Which hardware protocol or mock hardware should be used",
-	)
+    mount_arg = DeclareLaunchArgument(
+        name="mount",
+        default_value="mount_v1",
+        choices=["none", "mount_v1"],
+        description="Which mount to attach to the flange",
+    )
 
-	robot_desc_arg = DeclareLaunchArgument(
-		name="urdf",
-		default_value="base",
-		description="Robot Description file to use",
-		choices=["base", "mod"],
-	)
+    camera_arg = DeclareLaunchArgument(
+        name="camera",
+        default_value="realsense",
+        choices=["realsense", "oakd", "none"],
+        description="Which camera to attach to the mount",
+    )
 
-	load_base_arg = DeclareLaunchArgument(
-		name="load_base",
-		default_value="true",
-		description="Load the mobile robot model and tower",
-		choices=["true", "false"],
-	)
+    end_effector_arg = DeclareLaunchArgument(
+        name="end_effector",
+        default_value="toucher_v1",
+        choices=["toucher_v1", "none"],
+        description="Which end_effector to attach to the mount",
+    )
 
-	return LaunchDescription(
-		[
-			gripper_arg,
-			hardware_protocol_arg,
-			robot_desc_arg,
-			load_base_arg,
-			OpaqueFunction(function=launch_setup),
-		]
-	)
+    hardware_protocol_arg = DeclareLaunchArgument(
+        name="hardware_protocol",
+        default_value="simulation",
+        choices=["mock_hardware", "cri", "simulation", "ignition"],
+        description="Which hardware protocol or mock hardware should be used",
+    )
+
+    load_gazebo_arg = DeclareLaunchArgument(
+        name="load_gazebo",
+        default_value="false",
+        choices=["true", "false"],
+        description="Whether or not Gazebo Ignition is used",
+    )
+
+    jsp_gui_arg = DeclareLaunchArgument(
+        name="jsp_gui",
+        default_value="true",
+        choices=["true", "false"],
+        description="load joint state publisher gui to send joint states in ROS",
+    )
+
+    return LaunchDescription(
+        [
+            load_base_arg,
+            mount_arg,
+            camera_arg,
+            end_effector_arg,
+            hardware_protocol_arg,
+            load_gazebo_arg,
+            jsp_gui_arg,
+            OpaqueFunction(function=launch_setup),
+        ]
+    )
 
 
 def launch_setup(context, *args, **kwargs):
-	
-	if LaunchConfiguration("urdf").perform(context) == "mod":
-		robot_description_filename = "igus_rebel/igus_rebel_mod.urdf.xacro"
-	else:
-		robot_description_filename = "robot.urdf.xacro"
 
-	robot_description_file = PathJoinSubstitution(
-		[
-			FindPackageShare("igus_rebel_description_ros2"),
-			"urdf",
-			robot_description_filename,
-		]
-	)
+    # Sim time
+    if LaunchConfiguration("load_gazebo").perform(context) == 'true':
+        use_sim_time = True
+    else:
+        use_sim_time = False
 
-	# load robot description xacro
-	robot_description = Command(
-		[
-			FindExecutable(name="xacro"),
-			" ",
-			robot_description_file,
-			" hardware_protocol:=",
-			LaunchConfiguration("hardware_protocol"),
-			" gripper:=",
-			LaunchConfiguration("gripper"),
-			" load_base:=",
-			LaunchConfiguration("load_base"),
-		]
-	)
+    robot_description_filename = "robot.urdf.xacro"
 
-	# remap input and ouytput topics for robot state publisher and joint state publisher
-	remappings = [
-		("/joint_states", "/rebel/joint_states"),
-		("/robot_description", "/rebel/robot_description"),
-	]
+    robot_description_file = PathJoinSubstitution(
+        [
+            FindPackageShare("igus_rebel_description_ros2"),
+            "urdf",
+            robot_description_filename,
+        ]
+    )
 
-	# Nodes
-	robot_state_publisher_node = Node(
-		package="robot_state_publisher",
-		executable="robot_state_publisher",
-		name="robot_state_publisher",
-		parameters=[
-			{"robot_description": ParameterValue(robot_description, value_type=str)}
-		],
-		# subscribes to /rebel/joint_states and publishes to /rebel/robot_description
-		remappings=remappings
-	)
+    # load robot description xacro
+    robot_description = Command(
+        [
+            FindExecutable(name="xacro"),
+            " ",
+            robot_description_file,
+            " load_base:=",
+            LaunchConfiguration("load_base"),
+            " mount:=",
+            LaunchConfiguration("mount"),
+            " camera:=",
+            LaunchConfiguration("camera"),
+            " end_effector:=",
+            LaunchConfiguration("end_effector"),
+            " hardware_protocol:=",
+            LaunchConfiguration("hardware_protocol"),
+            " load_gazebo:=",
+            LaunchConfiguration("load_gazebo"),
+            " moveit:=",
+            "false",
+        ]
+    )
 
-	joint_state_publisher_gui_node = Node(
-		package="joint_state_publisher_gui",
-		executable="joint_state_publisher_gui",
-		name="joint_state_publisher_gui",
-		# publishes to /rebel/joint_states
-		remappings=remappings
-	)
+    # remap input and ouytput topics for robot state publisher and joint state publisher
+    remappings = [
+        ("/joint_states", "/rebel/joint_states"),
+        ("/robot_description", "/rebel/robot_description"),
+    ]
 
-	joint_state_publisher_node = Node(
-		package="joint_state_publisher",
-		executable="joint_state_publisher",
-		name="joint_state_publisher",
-		# publishes to /rebel/joint_states and receives URDF from /rebel/robot_description
-		remappings=remappings
-	)
+    # Nodes
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        parameters=[
+            {"robot_description": ParameterValue(robot_description, value_type=str)},
+            {'use_sim_time': use_sim_time},
+        ],
+        # subscribes to /rebel/joint_states and publishes to /rebel/robot_description
+        # remappings=remappings
+    )
 
-	if LaunchConfiguration("load_base").perform(context) == "true":
-		rviz_filename = "rebel_on_scout.rviz"
-	else:
-		rviz_filename = "rebel.rviz"
+    joint_state_publisher_gui_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        name="joint_state_publisher_gui",
+        parameters=[{'use_sim_time': use_sim_time}],
+        condition=IfCondition(LaunchConfiguration("jsp_gui")),
+        # publishes to /rebel/joint_states
+        # remappings=remappings
+    )
 
-	rviz_file = PathJoinSubstitution(
-		[FindPackageShare("igus_rebel_description_ros2"), "rviz", rviz_filename]
-	)
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
+        parameters=[{'use_sim_time': use_sim_time}],
+        # publishes to /rebel/joint_states and receives URDF from /rebel/robot_description
+        # remappings=remappings
+    )
 
-	rviz_node = Node(
-		package="rviz2",
-		executable="rviz2",
-		name="rviz2",
-		arguments=["-d", rviz_file],
-	)
+    # Ignition node
+    ignition_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            FindPackageShare(
+                "igus_rebel_gazebo_ignition"), '/launch', '/ignition.launch.py'
+        ]),
+        launch_arguments={
+            "use_sim_time": "True",
+            "moveit": "false"
+        }.items(),
+        condition=IfCondition(LaunchConfiguration("load_gazebo"))
+    )
 
-	return [
-		robot_state_publisher_node,
-		joint_state_publisher_gui_node,
-		joint_state_publisher_node,
-		rviz_node,
-	]
+    # RViz config files differ based on whether the base is loaded or not
+    if LaunchConfiguration("load_base").perform(context) == "true":
+        rviz_filename = "rebel_on_scout.rviz"
+    else:
+        rviz_filename = "rebel.rviz"
+
+    rviz_file = PathJoinSubstitution([
+        FindPackageShare("igus_rebel_description_ros2"),
+        "rviz", rviz_filename
+    ])
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", rviz_file],
+    )
+
+    return [
+        robot_state_publisher_node,
+        joint_state_publisher_gui_node,
+        joint_state_publisher_node,
+        ignition_launch,
+        rviz_node,
+    ]
