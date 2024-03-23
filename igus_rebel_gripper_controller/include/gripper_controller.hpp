1184341e4d1bd395fc31f6efe135eecfa9123829
SimonGiampy <simonegiampa99@gmail.com>
2024-03-23 20:20:51 +0100

    gripper hardware interface completed

--- igus_rebel_gripper_controller/include/gripper_controller.hpp
+++ igus_rebel_gripper_controller/include/gripper_controller.hpp
@@ -6,6 +6,12 @@
 // C++ imports
 #include <string>
 #include <vector>
+#include <fstream>
+
+// C imports for termios library
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
 
 // ROS2-Control imports
 #include <hardware_interface/actuator_interface.hpp>
@@ -16,36 +22,79 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp>
 
+// custom service definition
+#include "igus_rebel_gripper_controller/srv/gripper_actuation.hpp"
+
 namespace igus_rebel_gripper_controller {
 
 class GripperController : public hardware_interface::ActuatorInterface {
 
 public:
+	GripperController();
+
 	hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo &info) override;
 
-	LifecycleNodeInterface::CallbackReturn on_configure(const rclcpp_lifecycle::State &previous_state) override;
+	hardware_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State &previous_state) override;
 
 	std::vector<hardware_interface::StateInterface> export_state_interfaces(void) override;
 
 	std::vector<hardware_interface::CommandInterface> export_command_interfaces(void) override;
 
-	LifecycleNodeInterface::CallbackReturn on_activate(const rclcpp_lifecycle::State &previous_state) override;
+	hardware_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State &previous_state) override;
 
-	LifecycleNodeInterface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State &previous_state) override;
+	hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State &previous_state) override;
 
 	hardware_interface::return_type read(const rclcpp::Time &time, const rclcpp::Duration &period) override;
 
 	hardware_interface::return_type write(const rclcpp::Time &time, const rclcpp::Duration &period) override;
 
+	enum class GripperStates { GRIPPED, RELEASED, OFF };
+
 private:
+	
+	const std::vector<std::string> gripper_states = { "grip", "release", "off" };
+
+	rclcpp::Service<igus_rebel_gripper_controller::srv::GripperActuation>::SharedPtr grip_service_;
+
+	rclcpp::Node::SharedPtr node_;
+
+	/**
+	 * @brief Callback function for the gripper actuation service "/gripper_actuate"
+	 * @param request the request message containing the desired gripper state
+	 * @param response the response message containing the current gripper state
+	*/
+	void grip_service_callback(const std::shared_ptr<igus_rebel_gripper_controller::srv::GripperActuation::Request> request,
+							   const std::shared_ptr<igus_rebel_gripper_controller::srv::GripperActuation::Response> response);
+
+	/**
+	 * @brief Setup the serial communication via UART to the arduino controller
+	 * @return true if the serial communication was successfully setup, false otherwise
+	 */
+	bool setup_serial_comm(void);
+
+	rclcpp::Logger logger_;
+
+	// Serial port configuration (UART)
+	std::string serial_port; // serial port to the arduino controller
+	int baud_rate;			 // baud rate for the serial port
+	int serial_fd;			 // file descriptor for the serial port
+
 	// Store the command and state for the soft gripper
-	double hw_grip_cmd;
-	double hw_grip_state;
+	double *hw_grip_cmd;
+	double *hw_grip_state;
 
-	rclcpp::Logger logger_ = rclcpp::get_logger("igus_rebel::gripper_controller");
+	int grip_actuation_time;
 
+	// Store the command and state string equivalent for the soft gripper
+	std::string grip_cmd;
+	std::string grip_state;
+
+	// GPIO names in the ros2 control hardware interface
 	const std::string GPIO_CMD = "grip_cmd";
 	const std::string GPIO_STATE = "grip_state";
+
+	const std::string service_topic_ = "/gripper_actuate";
+	
 };
 
 } // namespace igus_rebel_gripper_controller
--- igus_rebel_gripper_controller/src/gripper_controller.cpp
+++ igus_rebel_gripper_controller/src/gripper_controller.cpp
@@ -3,14 +3,87 @@
 
 namespace igus_rebel_gripper_controller {
 
+GripperController::GripperController()
+	: logger_(rclcpp::get_logger("igus_rebel::GripperController")),
+	  serial_port("/dev/ttyUSB0"),
+	  baud_rate(B115200),
+	  serial_fd(-1),
+	  grip_actuation_time(1500) {
+
+	node_ = rclcpp::Node::make_shared("gripper_controller");
+}
+
+/**
+ * @brief Callback function for the gripper actuation service "/gripper_actuate"
+ * @param request the request message containing the desired gripper state
+ * @param response the response message containing the current gripper state
+ */
+void GripperController::grip_service_callback(
+	const std::shared_ptr<igus_rebel_gripper_controller::srv::GripperActuation::Request> request,
+	const std::shared_ptr<igus_rebel_gripper_controller::srv::GripperActuation::Response> /*response*/) {
+
+	// get the command from the service request
+	grip_cmd = request->command;
+
+	// check if the command is valid
+	if (gripper_states[static_cast<int>(GripperStates::GRIPPED)] != grip_cmd &&
+		gripper_states[static_cast<int>(GripperStates::RELEASED)] != grip_cmd &&
+		gripper_states[static_cast<int>(GripperStates::OFF)] != grip_cmd) {
+		RCLCPP_ERROR(logger_, "Invalid command received: %s", grip_cmd.c_str());
+		return;
+	}
+
+	// wait until grip_state is updated and equal to the command
+	while (grip_state != grip_cmd) {
+		// update the state
+		std::this_thread::sleep_for(std::chrono::milliseconds(20));
+	}
+
+	// wait for a predefined time to wait for the gripper to actuate
+	std::this_thread::sleep_for(std::chrono::milliseconds(grip_actuation_time));
+
+	// return from the service call to the client
+}
+
+/**
+ * @brief Setup the serial communication via UART to the arduino controller
+ * @return true if the serial communication was successfully setup, false otherwise
+ */
+bool GripperController::setup_serial_comm(void) {
+	serial_fd = open(serial_port.c_str(), O_RDWR | O_NOCTTY);
+	if (serial_fd == -1) {
+		return false;
+	}
+
+	// configure the serial UART communication
+	struct termios options;
+	tcgetattr(serial_fd, &options);
+	cfsetispeed(&options, baud_rate); // Set baud rate
+	cfsetospeed(&options, baud_rate);
+	options.c_cflag &= ~PARENB; // No parity
+	options.c_cflag &= ~CSTOPB; // 1 stop bit
+	options.c_cflag |= CS8;		// 8 data bits
+	tcsetattr(serial_fd, TCSANOW, &options);
+	return true;
+}
+
 hardware_interface::CallbackReturn GripperController::on_init(const hardware_interface::HardwareInfo &info) {
 	if (hardware_interface::ActuatorInterface::on_init(info) != hardware_interface::CallbackReturn::SUCCESS) {
 		return hardware_interface::CallbackReturn::ERROR;
 	}
 
+	// initialize the hardware interface command and state
+	*hw_grip_cmd = std::numeric_limits<double>::quiet_NaN();
+	*hw_grip_state = std::numeric_limits<double>::quiet_NaN();
+
+	// read the parameters from the ros2 control hardware interface
+	// and set the serial port and baud rate
+	baud_rate = stoi(info_.hardware_parameters["baud_rate"]);
+	serial_port = info_.hardware_parameters["serial_port"];
+
 	// GripperController has exactly one state and command interface for the GPIO
 	const hardware_interface::ComponentInfo &gpio = info_.gpios[0];
-	
+
 	if (gpio.command_interfaces.size() != 1) {
 		RCLCPP_ERROR(logger_, "GPIO '%s' has %zu command interfaces found. 1 expected.",
 					 gpio.name.c_str(), gpio.command_interfaces.size());
@@ -19,7 +92,7 @@ hardware_interface::CallbackReturn GripperController::on_init(const hardware_int
 
 	if (gpio.command_interfaces[0].name != GPIO_CMD) {
 		RCLCPP_ERROR(logger_, "Joint '%s' have %s command interfaces found. Only '%s' expected.",
-					 gpio.name.c_str(), gpio.command_interfaces[0].name.c_str(), GPIO_CMD);
+					 gpio.name.c_str(), gpio.command_interfaces[0].name.c_str(), GPIO_CMD.c_str());
 		return hardware_interface::CallbackReturn::ERROR;
 	}
 
@@ -31,20 +104,18 @@ hardware_interface::CallbackReturn GripperController::on_init(const hardware_int
 
 	if (gpio.state_interfaces[0].name != GPIO_STATE) {
 		RCLCPP_ERROR(logger_, "GPIO '%s' have %s state interface. Only '%s' expected.",
-					 gpio.name.c_str(), gpio.state_interfaces[0].name.c_str(), GPIO_STATE);
+					 gpio.name.c_str(), gpio.state_interfaces[0].name.c_str(), GPIO_STATE.c_str());
 		return hardware_interface::CallbackReturn::ERROR;
 	}
 
 	return hardware_interface::CallbackReturn::SUCCESS;
 }
 
-
-
 std::vector<hardware_interface::StateInterface> GripperController::export_state_interfaces() {
 	std::vector<hardware_interface::StateInterface> state_interfaces;
 
 	state_interfaces.emplace_back(hardware_interface::StateInterface(
-		info_.gpios[0].name, GPIO_STATE, &hw_grip_state));
+		info_.gpios[0].name, GPIO_STATE, hw_grip_state));
 
 	return state_interfaces;
 }
@@ -53,27 +124,67 @@ std::vector<hardware_interface::CommandInterface> GripperController::export_comm
 	std::vector<hardware_interface::CommandInterface> command_interfaces;
 
 	command_interfaces.emplace_back(hardware_interface::CommandInterface(
-		info_.gpios[0].name, GPIO_CMD, &hw_grip_cmd));
+		info_.gpios[0].name, GPIO_CMD, hw_grip_cmd));
 
 	return command_interfaces;
 }
 
+hardware_interface::CallbackReturn GripperController::on_configure(const rclcpp_lifecycle::State & /*previous_state*/) {
+
+	grip_service_ = node_->create_service<igus_rebel_gripper_controller::srv::GripperActuation>(
+		service_topic_, std::bind(&GripperController::grip_service_callback, this, std::placeholders::_1, std::placeholders::_2));
+
+	return hardware_interface::CallbackReturn::SUCCESS;
+}
+
 hardware_interface::CallbackReturn GripperController::on_activate(const rclcpp_lifecycle::State & /*previous_state*/) {
+	// set some default values for the state and command
+	if (std::isnan(*hw_grip_cmd)) {
+		*hw_grip_cmd = 0.0;
+	}
+	if (std::isnan(*hw_grip_state)) {
+		*hw_grip_state = 0.0;
+	}
+
+	// attempt to establish communication with the arduino controller
+	bool comm_established = setup_serial_comm();
+	if (!comm_established) {
+		RCLCPP_ERROR(logger_, "Failed to establish communication with the arduino controller.");
+		return hardware_interface::CallbackReturn::ERROR;
+	}
 
 	return hardware_interface::CallbackReturn::SUCCESS;
 }
 
 hardware_interface::CallbackReturn GripperController::on_deactivate(const rclcpp_lifecycle::State & /*previous_state*/) {
 
+	// close the serial port communication
+	if (serial_fd != -1) {
+		close(serial_fd);
+	}
+
 	return hardware_interface::CallbackReturn::SUCCESS;
 }
 
 hardware_interface::return_type GripperController::read(const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/) {
 
+	// no input from the serial port, skip
+
 	return hardware_interface::return_type::OK;
 }
 
 hardware_interface::return_type GripperController::write(const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/) {
+
+	if (grip_cmd == grip_state) {
+		// no new command received, skip
+		return hardware_interface::return_type::OK;
+	}
+
+	// write the command to the serial port
+	::write(serial_fd, grip_cmd.c_str(), grip_cmd.length());
+
+	// update the state
+	grip_state = grip_cmd;
 
 	return hardware_interface::return_type::OK;
 }
